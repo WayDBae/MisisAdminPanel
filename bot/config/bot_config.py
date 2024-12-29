7d3fe593671910e6982efad79dd333f4da13eec3
WayDBae <khayrakov.d@gmail.com>
2024-12-29 01:59:35 -0500

    some fix

--- bot/config/bot_config.py
+++ bot/config/bot_config.py
@@ -53,49 +53,12 @@ class Logger:
         return f"Logger(path={self.PATH}, type={self.TYPE})"
 
 
-class Bot:
-    def __init__(self):
-        self.TOKEN = env("BOT_TOKEN")
-        self.THREADED = env.bool("BOT_THREADED", default=False)
-        # Инициализация бота
-        self.bot = self.init_bot()
-
-    def init_bot(self):
-        """Инициализация подключения к боту."""
-        return TeleBot(token=self.TOKEN, threaded=self.THREADED)
-
-    def __str__(self):
-        return f"Bot(token={self.TOKEN}, threaded={self.THREADED})"
-
-
-class Weather:
-    def __init__(self):
-        self.URL = env("WEATHER_URL")
-        self.LANG = env("WEATHER_LANG", default="en")
-        self.CITY = env("WEATHER_CITY")
-        self.UNITS = env("WEATHER_UNITS", default="metric")
-        self.APPID = env("WEATHER_APPID")
-        self.weather = self.init_weather()
-
-    def init_weather(self):
-        """Формируем URL с подстановкой параметров"""
-        full_url = self.URL.format(self.CITY, self.UNITS, self.LANG, self.APPID)
-        return full_url
-
-    def __str__(self):
-        return f"Weather(url={self.URL}, city={self.CITY}, units={self.UNITS}, appid={self.APPID})"
-
-
 class Config:
     def __init__(self):
         # Инициализация отдельных конфигураций
         self.webhook = Webhook()
         self.database = PostgreSQL()
         self.logger = Logger()
-        self.bot = Bot()
-        self.DEVELOPER_CHAT_ID = env("DEVELOPER_CHAT_ID")
-        self.DEVELOPER_USERNAME = env("DEVELOPER_USERNAME")
-        self.weather = Weather()
 
     def __str__(self):
-        return f"Config(bot={self.bot}, logger={self.logger}, webhook={self.webhook}, database={self.database})"
+        return f"Config(logger={self.logger}, webhook={self.webhook}, database={self.database})"
