349489a72e38a4cb94ae19113e5b45aee7e65eed
Далер Хайраков <90933193+WayDBae@users.noreply.github.com>
2024-12-30 06:33:22 +0300

    Update settings.py

--- misis/settings.py
+++ misis/settings.py
@@ -36,7 +36,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "grappelli",
+    # "grappelli",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
